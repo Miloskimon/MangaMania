4ae5e3dd1c825cfa996e58338819c92205ef7b21
Lucas Miloski <lucasmiloski03@gmail.com>
2024-01-10 11:09:34 +0000

    first commit

---
+++ src/page/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    
+    <section>
+      <h1>Sobre mim</h1>
+      <hr />
+      <img src="#" alt="foto_minha" />
+      <p>Olá, me chamo Lucas Miloski de Oliveira, tenho 22 anos, Nacionou do Brasil.</p>
+    </section>
+  )
+}
