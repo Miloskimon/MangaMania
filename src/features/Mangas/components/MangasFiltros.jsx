f0e87595083cec35e6de94e659b8a93fe5769bdb
Lucas Miloski <lucasmiloski03@gmail.com>
2024-01-27 17:45:27 +0000

    change componente MangasFiltros

--- src/features/Mangas/components/MangasFiltros.jsx
+++ src/features/Mangas/components/MangasFiltros.jsx
@@ -1,13 +1,14 @@
 import { useDispatch } from "react-redux";
+import { getMangaApi } from "../MangaSlice";
 
 export default function MangasFiltros() {
 
     return(
         //filtros para aparecer os os mangas de acordo com o genero
         <section className="filtrosMangas">
-            <button onClick={() => {}}>Ação</button>
-            <button onClick={() => {}}>Romance</button>
+            <button onClick={() => {getMangaApi}}>all</button>
+            <button onClick={() => {getMangaApi.genres.action}}>Ação</button>
+            <button onClick={() => {getMangaApi.genres.raomance}}>Romance</button>
         </section>
-
     )
 }
