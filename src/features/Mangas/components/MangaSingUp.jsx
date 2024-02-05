9da53cf0d4d65c8db7944071bf03c796a6e0a16f
Lucas Miloski <lucasmiloski03@gmail.com>
2024-02-05 19:51:10 +0000

    MangaSingUP 2024/02/05

--- src/features/Mangas/components/MangaSingUp.jsx
+++ src/features/Mangas/components/MangaSingUp.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 export default function MangaSingUp(){
 
-    addEventListener('load', function(){
-        
-    let form = document.getElementById('registerform');
-
-    form.addEventListener('submit',function(event) {
+    const handleSubmit = (event) => {
 
         event.preventDefault();
 
         let nome = document.getElementById('nome').value;
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
-
 
         //para salvar os dados
         addStorage( nome, email, password);
@@ -24,28 +19,25 @@ export default function MangaSingUp(){
 
         //Confirmação cadastro
         alert('sucessfully registered');
-    })
+    };
 
-    function addStorage( nome, email, password) {
-        //pegar os dados
+    const addStorage = ( nome, email, password) => {
+        //pegar dados
         let registerform = JSON.parse(localStorage.getItem('registerform')) || [];
 
         //receber os codigos
         registerform.push({ nome, email, password });
 
         //salvar os dados
-        localStorage.setItem('registerform', JSON.stringify(registerform));        
-        
-    }
+        localStorage.setItem('registerform', JSON.stringify(registerform));
+    };
 
-
-})
 
     return(
             <section className="MangaSingUp">
                   <h1>Sing up</h1>
             
-                <form id='registerform'  >
+                <form id='registerform' onSubmit={handleSubmit} >
                     <label htmlFor="nome">Name: </label>
                     <input type="text" name="nome" id="nome"  required/>
                     <br />
@@ -53,9 +45,11 @@ export default function MangaSingUp(){
                     <label htmlFor="email">Email: </label>
                     <input type="email" name="email" id="email" required/>
                     <br />
+
                     <label htmlFor="password">Password: </label>
                     <input type="password" name="password" id="password" required/>
                     <br />
+                    
                     <button className='buttonRegister' type="submit">Register</button>
             
                 </form>
